0608c339c0baee8c48c5dc376ff5d415a631cec6
David Regnault <david>
2018-03-02 20:00:03 +0100

    02/03 20H

--- include/footer.php
+++ include/footer.php
@@ -24,7 +24,7 @@
 		<div class="footer_part">
             <ul class="footer_justify_left">
                 <li><a href="#">Faq</a></li>
-                <li><a href="#">Mentions légales</a></li>
+                <li><a href="mentions_legales_Legumalin.php">Mentions légales</a></li>
                 <li><a href="#">CGU</a></li>
                 <li><a href="plan_Legumalin.php">Plan du site</a></li>
             </ul>
