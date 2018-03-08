78917e8c5d572667324d898870e2675ef42a0279
David Regnault <david>
2018-03-08 18:50:31 +0100

    08/03 18h50

--- a-propos.php
+++ a-propos.php
@@ -18,26 +18,23 @@
 	<section>
 		<h3>La genèse de notre concept</h3>
 		<p>En 2017 en France, 3% de la population est végétarienne, soit 1,2 à 1,8 millions d’habitants et cette tendance va augmenter ces prochaines années car plus de 10% des français souhaitent adopter ce mode de nutrition dans les temps à venir. 
-		En plus des végétariens, de plus en plus de français décident de réduire leur consommation de viande.
-
-		Face à ces phénomènes, les légumes deviennent la base de l’alimentation.
-
+		En plus des végétariens, de plus en plus de français décident de réduire leur consommation de viande.<br />
+		Face à ces phénomènes, les légumes deviennent la base de l’alimentation.<br />
 		Pourtant, manger sainement est devenu un véritable défi quotidien car l’agriculture est devenue une production industrielle qui a pour objectif un rendement maximal malgré les conséquences sur notre santé et sur la qualité nutritive. 
-		En effet, la plupart des fruits et des légumes que nous mangeons, en particulier les pommes, les pêches, le raisin, les épinards, les concombres, la salade ou encore les petits pois (selon l'ONG américaine Environmental Working Group), sont remplis de pesticides.
-
-		La principale solution est donc de consommer des produits issus de l’agriculture biologique. Or, ce type de culture est couteux et, par conséquence, sont vendues 64% plus chers. Ils ne sont donc pas accessibles à tous.
+		En effet, la plupart des fruits et des légumes que nous mangeons, en particulier les pommes, les pêches, le raisin, les épinards, les concombres, la salade ou encore les petits pois (selon l'ONG américaine Environmental Working Group), sont remplis de pesticides.<br />
+		La principale solution est donc de consommer des produits issus de l’agriculture biologique. Or, ce type de culture est couteux et, par conséquence, sont vendues 64% plus chers. Ils ne sont donc pas accessibles à tous.<br />
 
 		Alors, une autre solution se présente à nous : la culture personnelle de son alimentation dans son potager.</p>
 	</section>
 	<section>
 		<h3>Legumalin, le potager chez son voisin</h3>
-		<p>Legumalin est une plateforme proposant à des propriétaires de jardins de laisser l’opportunité à des particuliers de jouir d’une partie de ce terrain à des particuliers afin de créer un potager. Les légumes cultivés devront obligatoirement respecter le principe de la culture biologique (pas d’engrais chimiques ou de pesticides).
+		<p>Legumalin est une plateforme proposant à des propriétaires de jardins de laisser l’opportunité à des particuliers de jouir d’une partie de ce terrain à des particuliers afin de créer un potager. Les légumes cultivés devront obligatoirement respecter le principe de la culture biologique (pas d’engrais chimiques ou de pesticides).<br />
 
-		Le partage de jardins ne sera pas payant mais le « jardinier » devra reverser un pourcentage de sa récolte au propriétaire afin de le faire profiter des produits de son jardin et de lui permettre, ainsi, de manger sainement.
+		Le partage de jardins ne sera pas payant mais le « jardinier » devra reverser un pourcentage de sa récolte au propriétaire afin de le faire profiter des produits de son jardin et de lui permettre, ainsi, de manger sainement.<br />
 
-		En plus d’en donner une partie au propriétaire, le jardinier a la possibilité de donner un pourcentage de sa récolte à une association caritative partenaire.
+		En plus d’en donner une partie au propriétaire, le jardinier a la possibilité de donner un pourcentage de sa récolte à une association caritative partenaire.<br />
 
-		Grâce à un système de géolocalisation, legumalin vous permets de trouver tous les jardins ou jardiniers à proximité et leurs préférences. 
+		Grâce à un système de géolocalisation, legumalin vous permets de trouver tous les jardins ou jardiniers à proximité et leurs préférences.<br />
 
 		N’attendez plus, rejoignez-nous.</p>
 	</section>
