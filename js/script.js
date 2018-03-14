2940ae5d1d6a29511f99217175ec22013e09e8f4
David Regnault <david>
2018-03-14 11:39:35 +0100

    Modifs

--- js/script.js
+++ js/script.js
@@ -17,53 +17,25 @@ var s ='open';
 				s ="open";
 			}	
 		});
-/* SCRIPT ANIMATION nav */
-
-var btn_nav = document.querySelector('#btn_nav');
-var span_nav = document.querySelector('#span_nav_bg');
-var nav = document.querySelector('nav');
-var u ='close';
-
-		btn_nav.addEventListener('click', function() {
-
-				nav.classList.remove("nav_mobile_fermer");
-				nav.classList.add("nav_mobile_ouvert");
-				u = "open";
-
-		});
-		span_nav.addEventListener('click', function() {
-
-				nav.classList.remove("nav_mobile_ouvert");
-				nav.classList.add("nav_mobile_fermer");
-				u ="close";
-		});
-
 
 /* SCRIPT cookies */
 
-var etatCookies = localStorage.getItem('etatCookies');
+var etatCookies;
 var refuserCookies = document.querySelector('#refuserCookies');
 var accepterCookies = document.querySelector('#accepterCookies');
 var cookies = document.querySelector('#cookies');
 
 if (etatCookies == "accepter") {
 		cookies.classList.add("displayNone");
-	} else if (etatCookies == "refuser") {
-		cookies.classList.add("displayNone");
-	} else if (etatCookies = null) {
-		cookies.classList.remove("displayNone");
-	} 
+	}
 
 	refuserCookies.addEventListener('click', function() {
-
-			localStorage.setItem("etatCookies", "refuser");
+			etatCookies = "accepter";
 			cookies.classList.add("displayNone");
 
 	});
 	accepterCookies.addEventListener('click', function() {
-
-			localStorage.setItem("etatCookies", "accepter");
 			cookies.classList.add("displayNone");
-
+			etatCookies = "accepter";
 	});
 
